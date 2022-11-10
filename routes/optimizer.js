9e756289722af7470e15f72c4defdccff64ce71e
WolkeLandgraf <66756114+WolkeLandgraf@users.noreply.github.com>
2022-11-10 20:57:49 -0300

    Primer version

---
+++ routes/optimizer.js
@@ -0,0 +1,149 @@
+const express = require('express')
+const router = express.Router()
+
+// Defino mi apikey en mi archivo .env en el root
+require('dotenv').config()
+
+const tinify = require("tinify");
+
+const apikey = process.env.NL_TINYPNG_APIKEY
+tinify.key = apikey;
+
+router.get("/", (req, res) => {
+  res.send('get optim')
+})
+
+router.post("/results", (req, res) => {
+
+  const cantidad = req.body.file.length
+
+  res.render('results', { fileList: req.body.file, ancho: req.body.ancho, cantidad: cantidad})
+})
+
+router.get("/compress", (req, res) => {
+
+  //requiring path and fs modules
+  const path = require('path');
+  const fs = require('fs');
+  //joining path of directory 
+  const directoryPath = path.join(__dirname, '../images');
+
+  const dirents = fs.readdirSync(directoryPath, { withFileTypes: true });
+  const files = dirents
+      .filter(dirent => dirent.isFile())
+      .map(dirent => dirent.name);
+
+  const cantidad = files.length
+
+  res.render("compress", { fileList: files, apikey: apikey, cantidad: cantidad })
+})
+
+
+router.post("/rename", (req, res) => {
+
+  const path = require('path');
+  const fs = require('fs');
+  //joining path of directory 
+  
+  const directoryPath = path.join(__dirname, '../images');
+
+  //passsing directoryPath and callback function
+  fs.readdir(directoryPath, function (err, files) {
+      //handling error
+      if (err) {
+          return console.log('Unable to scan directory: ' + err);
+      } 
+
+      //listing all files using forEach
+      files.forEach(function (file) {
+
+          let newName = file.replace(/[&\/\\#,+()$~%_'":*?<>{}\s+]/g,'-').toLowerCase();
+
+          if ( newName.charAt( 0 ) == '-' ) {
+            newName = newName.substring(1);
+          }
+          
+          if ( newName.includes("-.")) {
+            newName = newName.replace('-.','.');
+          }
+
+          if ( newName.includes("--")) {
+            newName = newName.replace('--','-');
+          }
+
+          if ( newName.includes("--")) {
+            newName = newName.replace('--','-');
+          }
+
+          if ( newName.includes(".-")) {
+            newName = newName.replace('.-','-');
+          }
+
+          if ( newName.includes("..")) {
+            newName = newName.replace('..','.');
+          }
+
+          // Quito tildes
+          newName = newName.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+
+          fs.rename(directoryPath+'/'+file, directoryPath+'/'+newName, () => {});
+      });
+
+      res.json({
+        done: 'its done',
+      });
+  })
+
+})
+
+
+router.post("/action", (req, res) => {
+
+  const fs = require("fs")
+  var stats = fs.statSync('./images/'+req.body.image)
+  var antes = formatBytes(stats.size)
+
+  var width = 1920;
+  if (req.body.ancho !== 'empty') {
+    width = req.body.ancho;
+  }
+
+  const source = tinify.fromFile('./images/'+req.body.image);
+  var resized = source.resize({
+      method: "scale",
+      width: parseInt(width),
+  });
+
+  resized.result().size(function(err, size) {
+
+    resized.toFile('./images-optimized/'+req.body.image);
+    var result = formatBytes(size)
+
+    var proximo = parseInt(req.body.id) + 1;
+
+    res.json({
+      antes: antes,
+      size: result,
+      id: req.body.id,
+      proximo: proximo,
+    });
+  });
+  
+})
+
+
+function formatBytes(bytes, decimals = 2) {
+  if (!+bytes) return '0 Bytes'
+
+  const k = 1024
+  const dm = decimals < 0 ? 0 : decimals
+  const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
+
+  const i = Math.floor(Math.log(bytes) / Math.log(k))
+
+  return `${parseFloat((bytes / Math.pow(k, i)).toFixed(dm))} ${sizes[i]}`
+}
+
+
+
+module.exports = router
---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const app = express()
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true}))
+
+app.set("view engine", "ejs")
+
+app.get("/", (req, res) => {
+
+  //requiring path and fs modules
+  const path = require('path');
+  const fs = require('fs');
+  //joining path of directory 
+  
+  const directoryPath = path.join(__dirname, '/images');
+
+  const dirents = fs.readdirSync(directoryPath, { withFileTypes: true });
+  const files = dirents
+      .filter(dirent => dirent.isFile())
+      .map(dirent => dirent.name);
+
+  res.render("index", { fileList: files })
+
+})
+
+const optimizerRouter = require('./routes/optimizer')
+app.use('/optimizer', optimizerRouter)
+
+
+app.listen(3000, () => { console.log('server started on port 3000')})
